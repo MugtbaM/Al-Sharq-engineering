b5fd1eb2fa115be9e8a346c52c24d2108bfcc6ea
MugtbaM <mmmhasy@gmail.com>
2025-09-03 17:58:41 +0200

    fixed responsive design

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -34,34 +34,3 @@ export const cardData = [
       link: "/previous-projects"
     }
   ];
-
-
-
-
-
-
-
-
-// export const cardData = [
-//     {
-//       title: "What We Do",
-//       image: "https://www.lloydslist.com/-/media/lloyds-list/images/stock-images/dry-bulk-cargoes/coal-unloading-lianyungang-coal-terminal-china.png?rev=60e6cb4286e143a9897d464be261c2ed",
-//     //   image: "https://images.unsplash.com/photo-1581091226033-d5c48150dbaa?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80",
-//       content: "We provide comprehensive engineering solutions and logistics services for industrial and commercial clients. Our expertise includes project management, industrial automation, supply chain optimization, and customized engineering solutions tailored to your specific needs.",
-//       link: "/what-we-do"
-//     },
-//     {
-//       title: "Our Vision",
-//       image: "https://images.unsplash.com/photo-1629089077520-c92fa3d532e5?q=80&w=1158&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//     //   image: "https://images.unsplash.com/photo-1504307651254-35680f356dfd?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80",
-//       content: "To be the leading provider of engineering and logistics solutions in the region, recognized for innovation, reliability, and excellence. We aim to transform industries through cutting-edge technology and efficient processes that drive sustainable growth for our clients.",
-//       link: "/our-vision"
-//     },
-//     {
-//       title: "Previous Projects",
-//       image: "https://media.istockphoto.com/id/531523399/photo/loading-of-coal-on-the-freighter.jpg?s=612x612&w=0&k=20&c=y8FDLrZ-JsVq9ZmvsVLobz-UeCeo-ORxBslOu9QvKmU=",
-//     //   image: "https://images.unsplash.com/photo-1464925257126-6450e871c667?ixlib=rb-4.0.3&auto=format&fit=crop&w=1000&q=80",
-//       content: "We have successfully delivered projects across various sectors including oil & gas, manufacturing, healthcare, and retail. Our portfolio includes the automation of a major production facility, logistics optimization for a retail chain, and the design of sustainable infrastructure solutions.",
-//       link: "/previous-projects"
-//     }
-//   ];
